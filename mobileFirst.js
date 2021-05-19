b769fd4547ff8c42ab108c08a9ff46584244df7e
99050823 <99050823@mydavinci.nl>
2021-05-19 11:49:48 +0200

    Week-9-blok-2
    11:49
    19-5-2021

---
+++ mobileFirst.js
@@ -0,0 +1,15 @@
+var icon = document.getElementById("icon");
+var dropdown = document.getElementById("dropdown");
+
+icon.onclick = iconClicked; 
+
+function iconClicked () {
+
+    if (dropdown.style.display == "none") {
+        dropdown.style.display = "block";
+        icon.style.color = "white";
+    } else {
+        dropdown.style.display = "none";
+        icon.style.color = "#3F3E3E";
+    }
+}
